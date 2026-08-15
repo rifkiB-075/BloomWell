3ab24dca8e21473a6de3ecdea973da2d4b22dc91
Zaza-404 <nazwaarifin316@gmail.com>
2026-08-15 16:35:15 +0700

    fixid

--- fiveserver.config.js
+++
@@ -1,7 +0,0 @@
-// Five-server configuration: set path to your php.exe if PHP is installed on Windows.
-// Example for XAMPP: "C:\\xampp\\php\\php.exe"
-// After editing, restart Five-server.
-
-module.exports = {
-  php: "C:\\xampp\\php\\php.exe"
-};
